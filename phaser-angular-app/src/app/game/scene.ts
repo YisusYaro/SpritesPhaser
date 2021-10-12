800bb9fb6bb77d1bb80b5d587152b10473dee41a
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2021-10-11 23:20:47 -0500

    music added

--- phaser-angular-app/src/app/game/scene.ts
+++ phaser-angular-app/src/app/game/scene.ts
@@ -13,13 +13,27 @@ export class Scene extends Phaser.Scene {
   ice_zombie?: GameObjects.Sprite;
   masked_orc?: GameObjects.Sprite;
   bd_scene?: GameObjects.Image;
+  start?: GameObjects.Image;
+  instruction?: GameObjects.Image;
 
   myCharacter?: GameObjects.Sprite;
+
+  characterNames: string[];
+  myCursor?: Phaser.Types.Input.Keyboard.CursorKeys;
+
+  isFirstClick = true;
+
+  music?: Phaser.Sound.BaseSound;
+
+  radians: number;
+
 
 
   constructor() {
     super({ key: "menu" });
-
+    this.characterNames = ['knight', 'necromancer', 'ice_zombie', 'masked_orc'];
+    this.isFirstClick = true;
+    this.radians = 0;
   }
 
   ngOnInit(): void {
@@ -27,8 +41,14 @@ export class Scene extends Phaser.Scene {
 
   preload() {
     this.load.path = "../../assets/";
+
+    this.load.image('start', 'fondoInicio.png');
+
     this.load.image('wood', 'wood.jpg');
     this.load.image('bg_scene', 'bg.png');
+    this.load.image('instruction', 'instruction.png');
+
+    this.load.audio('music', 'music.mp3');
 
 
     //portal
@@ -60,13 +80,29 @@ export class Scene extends Phaser.Scene {
 
   }
 
+  listenCursor(character: any, characterCursor: any) {
+    if (characterCursor.left.isDown) {
+      character.x--;
+      character.flipX = true;
+    }
+    if (characterCursor.right.isDown) {
+      character.x++;
+      character.flipX = false;
+    }
+    if (characterCursor.up.isDown) {
+      character.y--;
+    }
+    if (characterCursor.down.isDown) {
+      character.y++;
+    }
+  }
+
   setKeyboard() {
     const keyCodes = Phaser.Input.Keyboard.KeyCodes;
 
-    //this.myCursor = this.input.keyboard.createCursorKeys();
+    this.myCursor = this.input.keyboard.createCursorKeys();
 
   }
-
 
   create() {
 
@@ -76,6 +112,8 @@ export class Scene extends Phaser.Scene {
     //wood
     this.wood = this.add.image(this.scale.width / 2, this.scale.height / 2, 'wood');
 
+    const musicVolume = 0.09;
+    this.music = this.sound.add('music', { volume: musicVolume, loop: true });
 
     //portal
     this.portal = this.add.sprite(400, 150, 'portal').setScale(1).setInteractive();
@@ -84,7 +122,7 @@ export class Scene extends Phaser.Scene {
 
     //characters
     //knight
-    this.knight = this.add.sprite(120, 30, 'knight_idle').setScale(3).setInteractive();
+    this.knight = this.add.sprite(120, 35, 'knight_idle').setScale(3).setInteractive();
     this.input.setDraggable(this.knight);
     this.knight!.anims.play('knight_idle_anim');
     this.knight.setName('knight');
@@ -107,36 +145,71 @@ export class Scene extends Phaser.Scene {
     this.masked_orc!.anims.play('masked_orc_idle_anim');
     this.masked_orc.setName('masked_orc');
 
+    this.start = this.add.image(this.scale.width / 2, this.scale.height / 2, 'start');
 
     const events = Phaser.Input.Events;
 
 
+    this.input.on(events.POINTER_DOWN, () => {
+      if (this.isFirstClick) {
+        this.instruction = this.add.image(this.scale.width / 2, 300, 'instruction').setScale(0.3);
+        this.music!.play();
+        this.isFirstClick = false;
+        this.start!.destroy();
+        // this.start = new Date().getTime();
+      }
+    });
+
     this.input.on(events.DRAG_START, (pointer: Phaser.Input.Pointer, toucanPiece: GameObjects.Image) => {
       toucanPiece.setDepth(100);
-      
+
     });
 
     this.input.on(events.DRAG, (pointer: Phaser.Input.Pointer, character: GameObjects.Sprite, dragX: number, dragY: number) => {
       character.x = dragX;
       character.y = dragY;
-      character.setScale(4);  
+      character.setScale(4);
+      this.radians += 0.087;
+      character.setRotation(this.radians);
     });
 
     this.input.on(events.DRAG_END, (pointer: Phaser.Input.Pointer, character: GameObjects.Sprite, dropZone: boolean) => {
+      character.setDepth(0);
+      character.setScale(3);
+      character.setRotation(0);
       if (dropZone) {
-
-        alert(character.name);
 
         this.bd_scene = this.add.image(this.scale.width / 2, this.scale.height / 2, 'bg_scene');
 
-        //to refactor
+
         this.myCharacter = character;
         this.myCharacter.setDepth(100);
+        this.myCharacter.x = 100;
+        this.myCharacter.y = 200;
 
-        
+        //to refactor
+        if (this.myCharacter.name == 'knight') {
+          this.necromancer!.destroy();
+          this.ice_zombie!.destroy();
+          this.masked_orc!.destroy();
+        }
+        if (this.myCharacter.name == 'necromancer') {
+          this.knight!.destroy();
+          this.ice_zombie!.destroy();
+          this.masked_orc!.destroy();
+        }
+        if (this.myCharacter.name == 'ice_zombie') {
+          this.knight!.destroy();
+          this.necromancer!.destroy();
+          this.masked_orc!.destroy();
+        }
+        if (this.myCharacter.name == 'masked_orc') {
+          this.knight!.destroy();
+          this.necromancer!.destroy();
+          this.ice_zombie!.destroy();
+        }
       }
-      character.setDepth(0);
-      character.setScale(3);  
+
     });
 
     this.input.on(events.DROP, (pointer: Phaser.Input.Pointer, character: GameObjects.Image, dropZone: GameObjects.Image) => {
@@ -144,17 +217,17 @@ export class Scene extends Phaser.Scene {
       //let x = character.input.dragStartX;
       //let y = character.input.dragStartY;
 
-      character.setDepth(0);
-      character.setScale(3);  
+      // character.setDepth(0);
+      // character.setScale(3);  
 
     });
-
-
 
   }
 
   update(time: number, delta: number) {
-
+    if (this.myCharacter) {
+      this.listenCursor(this.myCharacter, this.myCursor);
+    }
   }
 
 
